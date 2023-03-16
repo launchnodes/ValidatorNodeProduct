c75e1b6696d339944289192d5a9481a28fe84e72
launchnodes <76775197+launchnodes@users.noreply.github.com>
2023-03-16 10:38:49 +0530

    Update TekuValidator.sh

--- Scripts/TekuValidator.sh
+++ Scripts/TekuValidator.sh
@@ -1,17 +1,27 @@
-echo "Enter the keystore file name: "
-read filename
-echo "Enter the validator_keys password: "
-read -s walletpassword
 sudo yum install nano -y
 sudo yum install unzip -y
 unzip -o validator_keys.zip
 sudo mkdir /home/ec2-user/validator
 sudo mv /home/ec2-user/validator_keys /home/ec2-user/validator
 sudo mkdir /home/ec2-user/validator/passwords
-echo $walletpassword > $filename.txt
-sudo mv /home/ec2-user/$filename.txt /home/ec2-user/validator/passwords
+echo Enter the number of keys:
+read num
+for ((i=1;i<=$num;i++)); do
+  while true; do
+    echo "Enter the name of keystore file $i (Exculde .json extension): "
+    read file_name
+    if [[ $file_name == *.json ]]; then
+        echo "Please enter file name without .json extension."
+    else
+        break
+    fi
+  done
+  echo "Enter the validator_keys password: "
+  read -s walletpassword 
+  echo "Creating file $file_name"
+  echo $walletpassword > $file_name.txt
+  sudo mv /home/ec2-user/$file_name.txt /home/ec2-user/validator/passwords
+done
 cd $Home
 sudo chmod -R 777 validator
-
-
 echo -e "\n\n\nHappy Staking, from Launchnodes..."
