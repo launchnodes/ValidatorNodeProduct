ade055d6e88a2ceda305972e10f76e2de63f8985
Launchnodes <jaya.preethi@launchnodes.com>
2023-04-13 11:03:09 +0530

    Added withdrawal scripts

--- Withdrawal_Scripts/Testnet_Broadcast.sh
+++ Withdrawal_Scripts/Testnet_Broadcast.sh
@@ -2,4 +2,3 @@ export PATH=$PATH:/usr/bin/curl
 echo "Enter the bls_to_execution.json file location: "
 read file
 echo "Braodcasting..."
-curl -X POST -H 'Content-type:application/json' -d @$file http://45.77.35.126:3500/eth/v1/beacon/pool/bls_to_execution_changes
