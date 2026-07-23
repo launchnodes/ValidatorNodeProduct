dacbe706d0e96930226d4f969f7651f32f162cc3
prithwishlaunchnodes <prithwish.c@launchnodes.com>
2026-07-23 14:18:50 +0530

    Update Broadcast.sh

--- Withdrawal_Scripts/Broadcast.sh
+++ Withdrawal_Scripts/Broadcast.sh
@@ -3,4 +3,4 @@ echo "Enter the bls_to_execution.json file location: "
 read file
 sudo chmod 777 $file
 echo "Braodcasting..."
-curl -X POST -H 'Content-type:application/json' -d @$file http://37.59.18.136:3500/eth/v1/beacon/pool/bls_to_execution_changes
+curl -X POST -H 'Content-type:application/json' -d @$file http://13.52.127.25:4000/eth/v1/beacon/pool/bls_to_execution_changes
